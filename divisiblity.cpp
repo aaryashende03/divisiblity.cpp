f6fc00363c5268e661f36f468efa694eda7442e0
aaryashende03 <168561498+aaryashende03@users.noreply.github.com>
2024-08-03 15:30:04 +0530

    Add files via upload

---
+++ divisiblity.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int number;
+	cout<<"Enter the number ="<<endl;
+	cin>>number;
+	
+	if(number%2==0)
+	{
+		cout<<"The number is divisible by 2"<<endl; //True condition
+	}
+	
+	else
+	{
+		cout<<"The number is not divisble by 2"<<endl; //Not Possible it will through an error
+	}
+	
+return 0;
+}
+	
